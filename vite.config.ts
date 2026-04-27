920709b814b6324c6538a645069eb242fe1f67fa
matelanae-byte <mate.lanae@gmail.com>
2026-04-27 23:58:25 +0300

    deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/cascade-ionic-rent-hub/",
   server: {
     host: "::",
     port: 8080,
